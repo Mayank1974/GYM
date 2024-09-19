11d3fbeccf56f422374df65fc69500088f8e700e
Mayank Chauhan <mayankchauhan1974@gmail.com>
2024-09-19 23:31:57 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     
-    <span className="p-3 flex justify-evenly bg-gray-900 text-white text-lg  shadow dark:bg-gray-900    sm:text-center dark:text-white">
+    <span className="p-3 flex flex-col bg-gray-900 text-white text-lg  shadow dark:bg-gray-900    sm:text-center dark:text-white">
       <span>
         © 2024{" "}
         <a href="/" className="hover:underline">
@@ -11,8 +11,8 @@ export default function Footer() {
         </a>
         . All Rights Reserved.
       </span>
-      |
-      <span>Designed & Developed by Mayank Chauhan</span>
+      
+      <span>Designed & Developed by<a href="https://www.linkedin.com/in/mayankchauhan2002/" className="font-bold">Mayank Chauhan</a></span>
     </span>
   );
 }
